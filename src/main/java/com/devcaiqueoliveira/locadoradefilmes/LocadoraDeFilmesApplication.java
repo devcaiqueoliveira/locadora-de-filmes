847826ba382518696a9c66cd545dfc487ce1dcad
Caique <devcaiqueoliveira@gmail.com>
2025-10-31 13:50:14 -0300

    feat: inicio do projeto com Spring

---
+++ src/main/java/com/devcaiqueoliveira/locadoradefilmes/LocadoraDeFilmesApplication.java
@@ -0,0 +1,13 @@
+package com.devcaiqueoliveira.locadoradefilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraDeFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraDeFilmesApplication.class, args);
+	}
+
+}
